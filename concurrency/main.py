0c387ab735981a75b6b3c987f84e3b8b335e7eaa
hemanthhari2000 <hemanthhari2000@gmail.com>
2025-02-14 18:16:24 +0530

    Refactor. separate main function and add it in multithreading module itself

--- concurrency/main.py
+++ concurrency/main.py
@@ -1,16 +1,8 @@
-from concurrency.multithreading import (
-    # manual_threading,
-    # real_world_example,
-    # synchronous_example,
-    thread_pool_executor,
-)
+from concurrency.multithreading.main import main as multithreading_example
 
 
 def main():
-    # synchronous_example()
-    # manual_threading()
-    thread_pool_executor()
-    # real_world_example()
+    multithreading_example()
 
 
 if __name__ == "__main__":
---
+++ concurrency/multithreading/main.py
@@ -0,0 +1,16 @@
+from .import (
+    # manual_threading,
+    # real_world_example,
+    # synchronous_example,
+    thread_pool_executor,
+)
+
+def main():
+    # synchronous_example()
+    # manual_threading()
+    thread_pool_executor()
+    # real_world_example()
+
+
+if __name__ == "__main__":
+    main()
